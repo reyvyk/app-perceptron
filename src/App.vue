f9afaa54783dc76635d0355fb68fa150881a8f92
reyvyk <reyvyk2@gmail.com>
2017-07-11 15:24:08 -0300

    Inicio do Projeto

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <div class="center">
+      <h1>Simule seu Credito</h1> <br>
+      <h3>Idade</h3> <br> <br>
+      <input type="radios">
+
+    </div>
+
+    <pre> {{ $data }} </pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      pessoas: [
+        { '18', '869,00', 1738, sim },
+        { '18', '869,00', 1738, sim },
+        { '18', '869,00', 1738, sim },
+        { '18', '869,00', 1738, sim },
+        { '18', '869,00', 1738, sim },
+        { '18', '869,00', 1738, sim },
+        { '18', '869,00', 1738, sim },
+        { '18', '869,00', 1738, sim },
+        { '18', '869,00', 1738, sim },
+        { '18', '869,00', 1738, sim },
+        { '18', '869,00', 1738, sim },
+        { '18', '869,00', 1738, sim },
+        { '18', '869,00', 1738, sim },
+      ]      
+    }
+  },
+  
+  methods: {
+    
+
+  },
+}
+</script>
+
+<style scoped>
+  .center {
+    width: 300px;
+    border: 1px solid #DDD;
+    margin: 0px auto;
+  }
+</style>
