aab152b7cdda0b97cf9f7ddf5282c82d834a5adf
Iosley Carlos Silva <iosley@outlook.com>
2017-07-12 11:41:32 -0300

    Perceptron criado com função de backup
    Criado para ser usado no lado do servidor

---
+++ server/redeNeural.js
@@ -0,0 +1,27 @@
+const fs                              = require('fs');
+const path                            = require('path');
+const { Architect, Trainer, Network } = require('synaptic');
+
+const backupFile = path.join(__dirname, 'myPerceptron.json');
+
+let myNetwork;
+
+try {
+  myNetwork = require(backupFile);
+  myNetwork = Network.fromJSON(myNetwork);
+} catch(err) {
+  myNetwork = new Architect.Perceptron(3, 6, 3, 2, 1);
+}
+
+myNetwork.save = function(filePath) {
+  filePath = filePath || backupFile;
+  return new Promise((reject, resolv) => {
+    fs.writeFile(filePath, JSON.stringify(this.toJSON()), (err) => {
+      if (err) return reject(err);
+      resolv();
+    });
+  });
+};
+
+module.exports = myNetwork;
+exports.default = module.exports;
